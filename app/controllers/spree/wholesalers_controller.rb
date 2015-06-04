0ec45a032bfc9e1e09c9d5a0f97629215fce5fce
Ishank Gupta <ishank_18@yahoo.com>
2015-06-04 17:07:47 +0530

    Fixed generator for spree 3; Changed to attr_accessible to strong parameters

--- app/controllers/spree/wholesalers_controller.rb
+++ app/controllers/spree/wholesalers_controller.rb
@@ -18,7 +18,7 @@ class Spree::WholesalersController < Spree::BaseController
   end
 
   def create
-    @wholesaler = Spree::Wholesaler.new(params[:wholesaler])
+    @wholesaler = Spree::Wholesaler.new(wholesale_params)
     if @wholesaler.save
       flash[:notice] = I18n.t('spree.wholesaler.signup_success')
       redirect_to spree.wholesalers_path
@@ -36,7 +36,7 @@ class Spree::WholesalersController < Spree::BaseController
   def update
     @wholesaler = Spree::Wholesaler.find(params[:id])
 
-    if @wholesaler.update_attributes(params[:wholesaler])
+    if @wholesaler.update_attributes(wholesale_params)
       flash[:notice] = I18n.t('spree.wholesaler.update_success')
     else
       flash[:error] = I18n.t('spree.wholesaler.update_failed')
@@ -50,4 +50,9 @@ class Spree::WholesalersController < Spree::BaseController
     flash[:notice] = I18n.t('spree.wholesaler.destroy_success')
     respond_with(@wholesaler)
   end
+
+  def wholesale_params
+    params.require(:wholesaler).permit(:ship_address, :bill_address, :company, :buyer_contact, :manager_contact, :phone, :fax, :resale_number, :taxid, :web_address, :terms, :notes, :use_billing, bill_address_attributes: [:firstname, :lastname, :address1, :address2, :city, :state_id, :zipcode, :country_id, :phone], ship_address_attributes: [:firstname, :lastname, :address1, :address2, :city, :state_id, :zipcode, :country_id, :phone], user_attributes: [:email, :password, :password_confirmation])
+  end
+
 end
--- lib/generators/spree_wholesale/install_generator.rb
+++ lib/generators/spree_wholesale/install_generator.rb
@@ -7,13 +7,13 @@ module SpreeWholesale
       desc "Installs required stylesheets, javascripts and migrations for spree_wholesale"
 
       def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require wholesaler_address\n"
-        append_file "app/assets/javascripts/admin/all.js", "//= require wholesaler_address\n"
+        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require wholesaler_address\n"
+        append_file "vendor/assets/javascripts/spree/backend/all.js", "//= require wholesaler_address\n"
       end
 
       def add_stylesheets
-        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/wholesale\n", :before => /\*\//, :verbose => true
-        inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/wholesalers\n", :before => /\*\//, :verbose => true
+        inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", " *= require store/wholesale\n", :before => /\*\//, :verbose => true
+        inject_into_file "vendor/assets/stylesheets/spree/backend/all.css", " *= require admin/wholesalers\n", :before => /\*\//, :verbose => true
       end
 
       def install_migrations
