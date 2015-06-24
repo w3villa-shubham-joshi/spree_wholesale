7d2c76e1e80b4478d693dbee8c55786ac94994d1
Ishank Gupta <ishank_18@yahoo.com>
2015-06-24 17:53:30 +0530

    Added deface for product delivery prices

---
+++ app/overrides/delivery_product_price.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(:virtual_path => "spree/checkout/_delivery", 
+                     :name => "delivery_product_price", 
+                     :replace => ".stock-contents .item-price", 
+                     :text => "<td class='item-price'>
+                                <%= spree_current_user.has_spree_role?(:wholesaler) ? number_to_currency(item.variant.product.wholesale_price) : display_price(item.variant) %>
+                              </td>")
